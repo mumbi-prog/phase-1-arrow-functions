01b6acc39fa38eb019468678afd3b6f3b056832f
Sylvia Mumbi <mumbi099@gmail.com>
2023-04-04 03:39:48 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+//Task
+const divide = () => 2000 / 100
+
+//Task2
+const square =(x)=>{
+    return x*x;
+}
+console.log(square(4));
+
+//Task3
+const add = (a, b) => {
+    return a+b;
+}
+console.log(add(3, 4));
